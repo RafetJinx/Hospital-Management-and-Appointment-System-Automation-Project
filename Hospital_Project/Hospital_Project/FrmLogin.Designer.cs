b28e23df82baae6053c04ad15c1337c81e4a887b
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-01-04 14:59:53 +0300

    Created form Login.cs
    Created form Login.cs

---
+++ Hospital_Project/Hospital_Project/FrmLogin.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Hospital_Project
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPatient = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDoctor = new System.Windows.Forms.Button();
+            this.btnSecretary = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPatient
+            // 
+            this.btnPatient.Location = new System.Drawing.Point(74, 325);
+            this.btnPatient.Name = "btnPatient";
+            this.btnPatient.Size = new System.Drawing.Size(100, 42);
+            this.btnPatient.TabIndex = 1;
+            this.btnPatient.Text = "Hasta";
+            this.btnPatient.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ink Free", 32.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(390, 53);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pistipist Hastanesi";
+            // 
+            // btnDoctor
+            // 
+            this.btnDoctor.Location = new System.Drawing.Point(290, 325);
+            this.btnDoctor.Name = "btnDoctor";
+            this.btnDoctor.Size = new System.Drawing.Size(100, 42);
+            this.btnDoctor.TabIndex = 4;
+            this.btnDoctor.Text = "Doktor";
+            this.btnDoctor.UseVisualStyleBackColor = true;
+            // 
+            // btnSecretary
+            // 
+            this.btnSecretary.Location = new System.Drawing.Point(506, 325);
+            this.btnSecretary.Name = "btnSecretary";
+            this.btnSecretary.Size = new System.Drawing.Size(100, 42);
+            this.btnSecretary.TabIndex = 6;
+            this.btnSecretary.Text = "Sekreter";
+            this.btnSecretary.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Hospital_Project.Properties.Resources.pistipist_rank;
+            this.pictureBox5.Location = new System.Drawing.Point(454, 7);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(210, 146);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Hospital_Project.Properties.Resources.secretary;
+            this.pictureBox3.Location = new System.Drawing.Point(454, 173);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(210, 146);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Hospital_Project.Properties.Resources.doctor;
+            this.pictureBox2.Location = new System.Drawing.Point(238, 173);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(210, 146);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.GhostWhite;
+            this.pictureBox1.Image = global::Hospital_Project.Properties.Resources.patient;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 173);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(682, 380);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.btnSecretary);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnDoctor);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPatient);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "FrmLogin";
+            this.Text = "Giriş Sayfası";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPatient;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDoctor;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnSecretary;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
