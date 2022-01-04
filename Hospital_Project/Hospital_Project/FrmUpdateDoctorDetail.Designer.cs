188e464d164f88947a0b87a563d9d30a70682615
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-01-04 15:41:47 +0300

    Created form UpdateDoctorDetail.cs
    Created form UpdateDoctorDetail.cs (Design)

---
+++ Hospital_Project/Hospital_Project/FrmUpdateDoctorDetail.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Hospital_Project
+{
+    partial class FrmUpdateDoctorDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.msktxtTC = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.cbBranch = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnUpdateDoctorDetail = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Ink Free", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(10, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(606, 46);
+            this.label7.TabIndex = 41;
+            this.label7.Text = "Doktor Bilgilerini Güncelleme Ekrani";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(321, 70);
+            this.txtName.Margin = new System.Windows.Forms.Padding(5);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(137, 31);
+            this.txtName.TabIndex = 40;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(321, 111);
+            this.txtLastName.Margin = new System.Windows.Forms.Padding(5);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(137, 31);
+            this.txtLastName.TabIndex = 39;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(132, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 23);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "Brans:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(132, 119);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 23);
+            this.label4.TabIndex = 36;
+            this.label4.Text = "Soyad:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 78);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 23);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Ad:";
+            // 
+            // msktxtTC
+            // 
+            this.msktxtTC.Location = new System.Drawing.Point(321, 152);
+            this.msktxtTC.Margin = new System.Windows.Forms.Padding(5);
+            this.msktxtTC.Mask = "00000000000";
+            this.msktxtTC.Name = "msktxtTC";
+            this.msktxtTC.Size = new System.Drawing.Size(137, 31);
+            this.msktxtTC.TabIndex = 34;
+            this.msktxtTC.ValidatingType = typeof(int);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 160);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 23);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "TC Kimlik Numarasi:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(132, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 23);
+            this.label3.TabIndex = 42;
+            this.label3.Text = "Sifre:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(321, 228);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(137, 31);
+            this.txtPassword.TabIndex = 43;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // cbBranch
+            // 
+            this.cbBranch.FormattingEnabled = true;
+            this.cbBranch.Location = new System.Drawing.Point(321, 191);
+            this.cbBranch.Name = "cbBranch";
+            this.cbBranch.Size = new System.Drawing.Size(137, 31);
+            this.cbBranch.TabIndex = 44;
+            // 
+            // btnUpdateDoctorDetail
+            // 
+            this.btnUpdateDoctorDetail.Location = new System.Drawing.Point(321, 265);
+            this.btnUpdateDoctorDetail.Name = "btnUpdateDoctorDetail";
+            this.btnUpdateDoctorDetail.Size = new System.Drawing.Size(137, 59);
+            this.btnUpdateDoctorDetail.TabIndex = 45;
+            this.btnUpdateDoctorDetail.Text = "Bilgileri Guncelle";
+            this.btnUpdateDoctorDetail.UseVisualStyleBackColor = true;
+            // 
+            // FrmUpdateDoctorDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(628, 344);
+            this.Controls.Add(this.btnUpdateDoctorDetail);
+            this.Controls.Add(this.cbBranch);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.msktxtTC);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "FrmUpdateDoctorDetail";
+            this.Text = "FrmUpdateDoctorDetail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox msktxtTC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.ComboBox cbBranch;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnUpdateDoctorDetail;
+    }
+}
